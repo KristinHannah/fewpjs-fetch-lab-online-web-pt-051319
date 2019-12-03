42ae11e6544ea5cebc7d5db387f188d07818ea2b
Kristin Kraemer <kristinhannahk@gmail.com>
2019-12-03 15:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books', {mode: 'no-cors'})
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -14,3 +16,4 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
